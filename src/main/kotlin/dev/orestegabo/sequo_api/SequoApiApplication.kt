cda460afa65d9d8b5c6e7bedc260ad623d23f86c
oreste <muhirwa.g.oreste@gmail.com>
2026-07-26 04:12:07 +0200

    first commit

---
+++ src/main/kotlin/dev/orestegabo/sequo_api/SequoApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.orestegabo.sequo_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SequoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SequoApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/orestegabo/sequo_api/SequoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.orestegabo.sequo_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SequoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
