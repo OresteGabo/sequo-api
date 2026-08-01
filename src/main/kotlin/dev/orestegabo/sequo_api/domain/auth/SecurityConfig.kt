50816fd13635fefaa63e32b8f4eca55c3a97cdfe
oreste <muhirwa.g.oreste@gmail.com>
2026-08-01 17:08:59 +0200

    Update SecurityConfig.kt to provide a BCryptPasswordEncoder bean for Sequo API

--- src/main/kotlin/dev/orestegabo/sequo_api/domain/auth/SecurityConfig.kt
+++ src/main/kotlin/dev/orestegabo/sequo_api/domain/auth/SecurityConfig.kt
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
@@ -13,6 +15,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter
 ) {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
