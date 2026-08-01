eb743d9d95c0d18705995c9ab335e48c4cd14601
oreste <muhirwa.g.oreste@gmail.com>
2026-08-01 17:09:26 +0200

    Add UserRepository to manage user data access with lookups by email, provider, and reset token for Sequo API

---
+++ src/main/kotlin/dev/orestegabo/sequo_api/domain/auth/UserRepository.kt
@@ -0,0 +1,10 @@
+package dev.orestegabo.sequo_api.domain.auth
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface UserRepository : JpaRepository<User, String> {
+    fun findByEmail(email: String): User?
+    fun findByProviderAndProviderId(provider: AuthProvider, providerId: String): User?
+    fun findByResetToken(resetToken: String): User?
+}
