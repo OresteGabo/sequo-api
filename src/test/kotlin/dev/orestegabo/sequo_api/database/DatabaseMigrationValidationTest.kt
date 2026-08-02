379821fc8fb14133eb8787c95620837f7c304a0c
oreste <muhirwa.g.oreste@gmail.com>
2026-08-02 15:02:04 +0200

    Add integration tests to DatabaseMigrationValidationTest to verify schema creation and constraint integrity for delivery fulfillment tables

--- src/test/kotlin/dev/orestegabo/sequo_api/database/DatabaseMigrationValidationTest.kt
+++ src/test/kotlin/dev/orestegabo/sequo_api/database/DatabaseMigrationValidationTest.kt
@@ -1,7 +1,12 @@
 package dev.orestegabo.sequo_api.database
 
+import org.springframework.dao.DataAccessException
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 @SpringBootTest(
     properties = [
@@ -10,10 +15,101 @@ import kotlin.test.Test
         "spring.flyway.enabled=true"
     ]
 )
-class DatabaseMigrationValidationTest {
+class DatabaseMigrationValidationTest @Autowired constructor(
+    private val jdbcTemplate: JdbcTemplate,
+) {
 
     @Test
     fun flywaySchemaMatchesCurrentJpaEntities() {
         // Spring context startup performs Flyway migration and Hibernate schema validation.
+    }
+
+    @Test
+    fun deliveryFulfillmentTablesAreMigrated() {
+        val expectedTables = listOf(
+            "MERCHANT_SUB_ORDERS",
+            "DELIVERY_MISSIONS",
+            "DELIVERY_PINS",
+            "RELAY_PARCELS",
+            "RELAY_PICKUP_CODES",
+            "RELAY_CUSTODY_EVENTS",
+        )
+
+        expectedTables.forEach { tableName ->
+            assertEquals(
+                1,
+                jdbcTemplate.queryForObject(
+                    """
+                    select count(*)
+                    from information_schema.tables
+                    where table_schema = 'PUBLIC'
+                      and table_name = ?
+                    """.trimIndent(),
+                    Int::class.java,
+                    tableName,
+                ),
+                "$tableName should be created by Flyway.",
+            )
+        }
+    }
+
+    @Test
+    fun merchantSubOrderStatusConstraintRejectsInvalidState() {
+        assertFailsWith<DataAccessException> {
+            jdbcTemplate.update(
+                """
+                insert into merchant_sub_orders (
+                    id,
+                    sub_order_code,
+                    order_id,
+                    merchant_id,
+                    status
+                ) values (?, ?, ?, ?, ?)
+                """.trimIndent(),
+                "sub-order-invalid",
+                "SC-INVALID",
+                "order-1",
+                "merchant-1",
+                "PACKED_WITHOUT_ACCEPTANCE",
+            )
+        }
+    }
+
+    @Test
+    fun deliveryPinsMustReferenceExistingMission() {
+        assertFailsWith<DataAccessException> {
+            jdbcTemplate.update(
+                """
+                insert into delivery_pins (
+                    id,
+                    delivery_mission_id,
+                    pin_hash,
+                    expires_at
+                ) values (?, ?, ?, current_timestamp)
+                """.trimIndent(),
+                "pin-orphan",
+                "missing-mission",
+                "hashed-pin",
+            )
+        }
+    }
+
+    @Test
+    fun relayPickupCodesMustReferenceExistingParcel() {
+        assertFailsWith<DataAccessException> {
+            jdbcTemplate.update(
+                """
+                insert into relay_pickup_codes (
+                    id,
+                    relay_parcel_id,
+                    code_hash,
+                    expires_at
+                ) values (?, ?, ?, current_timestamp)
+                """.trimIndent(),
+                "relay-code-orphan",
+                "missing-parcel",
+                "hashed-code",
+            )
+        }
     }
 }
