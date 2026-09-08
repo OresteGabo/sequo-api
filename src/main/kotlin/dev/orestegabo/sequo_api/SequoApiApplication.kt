b75f00d2e6aafef64165924a0bc46e39cb8cf863
oreste <muhirwa.g.oreste@gmail.com>
2026-09-08 22:20:34 +0200

    Enable scheduled task execution for Sequo API by adding @EnableScheduling to SequoApiApplication

--- src/main/kotlin/dev/orestegabo/sequo_api/SequoApiApplication.kt
+++ src/main/kotlin/dev/orestegabo/sequo_api/SequoApiApplication.kt
@@ -2,8 +2,10 @@ package dev.orestegabo.sequo_api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class SequoApiApplication
 
 fun main(args: Array<String>) {
