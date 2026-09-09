2d4eb812a8f937a0d7a7c26da4d0f13ef8f12fc2
oreste <muhirwa.g.oreste@gmail.com>
2026-09-09 05:09:51 +0200

    Update `OperationalSchedulersTest` to verify that `DeliveryReadinessDispatchScheduler` is disabled by default for Sequo API

--- src/test/kotlin/dev/orestegabo/sequo_api/domain/operations/OperationalSchedulersTest.kt
+++ src/test/kotlin/dev/orestegabo/sequo_api/domain/operations/OperationalSchedulersTest.kt
@@ -17,6 +17,7 @@ class OperationalSchedulersTest @Autowired constructor(
         assertFalse(context.containsBean("notificationOutboxScheduler"))
         assertFalse(context.containsBean("relayDelayedParcelScheduler"))
         assertFalse(context.containsBean("deliveryMissionExpiryScheduler"))
+        assertFalse(context.containsBean("deliveryReadinessDispatchScheduler"))
         assertFalse(context.containsBean("merchantFulfillmentSlaWarningScheduler"))
         assertFalse(context.containsBean("settlementEligibilityScheduler"))
         kotlin.runCatching { context.getBean(NotificationOutboxScheduler::class.java) }
@@ -24,6 +25,9 @@ class OperationalSchedulersTest @Autowired constructor(
             .onFailure { assertTrue(it is NoSuchBeanDefinitionException) }
         kotlin.runCatching { context.getBean(DeliveryMissionExpiryScheduler::class.java) }
             .onSuccess { error("Delivery mission expiry scheduler should be disabled by default.") }
+            .onFailure { assertTrue(it is NoSuchBeanDefinitionException) }
+        kotlin.runCatching { context.getBean(DeliveryReadinessDispatchScheduler::class.java) }
+            .onSuccess { error("Delivery readiness dispatch scheduler should be disabled by default.") }
             .onFailure { assertTrue(it is NoSuchBeanDefinitionException) }
         kotlin.runCatching { context.getBean(MerchantFulfillmentSlaWarningScheduler::class.java) }
             .onSuccess { error("Merchant fulfillment SLA warning scheduler should be disabled by default.") }
