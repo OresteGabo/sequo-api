0fdd1532110acede59226b5859b02f7dfc9cf6a4
oreste <muhirwa.g.oreste@gmail.com>
2026-09-09 03:26:35 +0200

    Update OperationalSchedulersTest to include deliveryMissionExpiryScheduler in default-disabled checks for Sequo API

--- src/test/kotlin/dev/orestegabo/sequo_api/domain/operations/OperationalSchedulersTest.kt
+++ src/test/kotlin/dev/orestegabo/sequo_api/domain/operations/OperationalSchedulersTest.kt
@@ -16,9 +16,13 @@ class OperationalSchedulersTest @Autowired constructor(
     fun `operational schedulers are disabled by default`() {
         assertFalse(context.containsBean("notificationOutboxScheduler"))
         assertFalse(context.containsBean("relayDelayedParcelScheduler"))
+        assertFalse(context.containsBean("deliveryMissionExpiryScheduler"))
         assertFalse(context.containsBean("settlementEligibilityScheduler"))
         kotlin.runCatching { context.getBean(NotificationOutboxScheduler::class.java) }
             .onSuccess { error("Notification scheduler should be disabled by default.") }
+            .onFailure { assertTrue(it is NoSuchBeanDefinitionException) }
+        kotlin.runCatching { context.getBean(DeliveryMissionExpiryScheduler::class.java) }
+            .onSuccess { error("Delivery mission expiry scheduler should be disabled by default.") }
             .onFailure { assertTrue(it is NoSuchBeanDefinitionException) }
     }
 }
